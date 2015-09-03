cac70e88f1d8dd82f229979d78e6390ad435d004
J. Pablo Fernández <pupeno@carouselapps.com>
2015-09-03 15:37:12 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
-(defproject jar-copier "0.1.0-SNAPSHOT"
+(defproject jar-copier "0.1.0"
   :description "Copy a jar from your dependencies into your resources."
   :url "https://github.com/carouselapps/jar-copier"
   :license {:name "Eclipse Public License"
