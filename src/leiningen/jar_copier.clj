83315d67f8cde0661395fe53f543c0ed4e7b0e34
J. Pablo Fernández <pupeno@carouselapps.com>
2015-11-18 10:32:15 +0000

    Copy non-java agent jars as well and thorough testing for that case as well as misconfigurations. Closes #1.

--- src/leiningen/jar_copier.clj
+++ src/leiningen/jar_copier.clj
@@ -5,23 +5,29 @@
             [cemerick.pomegranate.aether :as aether]
             [clojure.java.io :as io]))
 
-(defn- copy-java-agents [java-agents destination]
+(defn- copy-jars [jars destination]
   (doseq [[jar-file full-destination] (map list
-                                           (aether/dependency-files (aether/resolve-dependencies :coordinates java-agents))
-                                           (map #(io/file destination (str (first %) ".jar")) java-agents))]
+                                           (aether/dependency-files (aether/resolve-dependencies :coordinates jars))
+                                           (map #(io/file destination (str (first %) ".jar")) jars))]
     (lein/info "Coping" (.getPath jar-file) "to" (.getPath full-destination))
     (io/make-parents full-destination)
     (io/copy jar-file full-destination)))
 
-(def misses? (complement contains?))
+(def ^{:private true} misses? (complement contains?))
 
 (defn jar-copier "Copy a jar from your dependencies into your resources."
   [project & args]
   (if (misses? project :jar-copier)
-    (lein/warn "jar-copier is not configured.")
+    (lein/warn "jar-copier is not configured. Your Leiningen project needs a :jar-copier entry.")
     (if (misses? (:jar-copier project) :destination)
-      (lein/warn "jar-copier is missing the destination configuration")
-      (if (and (get-in project [:jar-copier :java-agents])
-               (misses? project :java-agents))
-        (lein/warn "jar-copier requested to copy java agents, but no java agents were present.")
-        (copy-java-agents (:java-agents project) (get-in project [:jar-copier :destination]))))))
+      (lein/warn "jar-copier is missing the destination configuration. You :jar-copier entry needs a :destination value.")
+      (if (and (misses? (:jar-copier project) :jars)
+               (misses? (:jar-copier project) :java-agents))
+        (lein/warn "jar-copier has nothing to copy, you need to specify :jars or :java-agents (or both).")
+        (let [destination (get-in project [:jar-copier :destination])]
+          (if (and (get-in project [:jar-copier :java-agents])
+                   (misses? project :java-agents))
+            (lein/warn "jar-copier requested to copy java agents, but no java agents were specified in your project.")
+            (copy-jars (:java-agents project) destination))
+          (when (contains? (:jar-copier project) :jars)
+            (copy-jars (get-in project [:jar-copier :jars]) destination)))))))
--- test/leiningen/jar_copier_test.clj
+++ test/leiningen/jar_copier_test.clj
@@ -5,8 +5,50 @@
             [leiningen.jar-copier :refer :all]
             [clojure.java.io :as io]))
 
-(deftest jar-copier-test
+(deftest no-jar-copier-config-issues-warning
+  (let [warnings (atom 0)]
+    (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
+      (jar-copier {}))
+    (is (= 1 @warnings))))
+
+(deftest no-destination-issues-warning
+  (let [warnings (atom 0)]
+    (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
+      (jar-copier {:jar-copier {}}))
+    (is (= 1 @warnings))))
+
+(deftest nothing-to-copy-issues-warning
+  (let [warnings (atom 0)]
+    (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
+      (jar-copier {:jar-copier {:destination "target/jars"}}))
+    (is (= 1 @warnings))))
+
+(deftest lack-of-java-agents-issues-warning
+  (let [warnings (atom 0)]
+    (with-redefs [leiningen.core.main/warn (fn [& _] (swap! warnings inc))]
+      (jar-copier {:jar-copier {:java-agents true
+                                 :destination "target/jars"}}))
+    (is (= 1 @warnings))))
+
+(deftest copy-java-agent
   (jar-copier {:java-agents '[[org.clojure/clojure "1.7.0"]]
-               :jar-copier {:java-agents true
+               :jar-copier  {:java-agents true
+                             :destination "target/jars"}})
+  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
+  (io/delete-file "target/jars/org.clojure/clojure.jar"))
+
+(deftest copy-plain-jar
+  (jar-copier {:jar-copier {:jars        '[[org.clojure/clojure "1.7.0"]]
                             :destination "target/jars"}})
-  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar"))))
+  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
+  (io/delete-file "target/jars/org.clojure/clojure.jar"))
+
+(deftest copy-plain-jar-and-java-agents
+  (jar-copier {:java-agents '[[org.clojure/clojure "1.7.0"]]
+               :jar-copier  {:jars        '[[org.clojure/clojure-contrib "1.2.0"]]
+                             :java-agents true
+                             :destination "target/jars"}})
+  (is (.exists (io/as-file "target/jars/org.clojure/clojure.jar")))
+  (is (.exists (io/as-file "target/jars/org.clojure/clojure-contrib.jar")))
+  (io/delete-file "target/jars/org.clojure/clojure.jar")
+  (io/delete-file "target/jars/org.clojure/clojure-contrib.jar"))
