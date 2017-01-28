d8f2619fc7ccb63d1740ce980f2cc57490f97ad8
J. Pablo Fernández <pupeno@pupeno.com>
2017-01-28 13:33:59 +0000

    Version 0.3.1, just to point out upcoming name change.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
-(defproject com.carouselapps/jar-copier "0.3.0"
-  :description "Copy a jar from your dependencies into your resources."
-  :url "https://carouselapps.com/jar-copier/"
+(defproject com.carouselapps/jar-copier "0.3.1"
+  :description "Copy a jar from your dependencies into your resources. This library changed namespace to com.pupeno/jar-copier"
+  :url "https://github.com/pupeno/jar-copier"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
   :signing {:gpg-key "F2FB1C6F"}
   :scm {:name "git"
-        :url  "https://github.com/carouselapps/jar-copier"}
+        :url  "https://github.com/pupeno/jar-copier"}
   :eval-in-leiningen true)
